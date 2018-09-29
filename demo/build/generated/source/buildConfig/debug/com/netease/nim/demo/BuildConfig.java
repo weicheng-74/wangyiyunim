6127b1dff64050a83a7af5f57330bbfdfd8e0a2c
twc <twc@twcdeMacBook-Pro.local>
2018-09-29 16:25:24 +0800

    Initial commit

---
+++ demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.netease.nim.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.netease.nim.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 58;
+  public static final String VERSION_NAME = "5.6.1";
+  // Fields from default config.
+  public static final String BUILD_DATE = "2018-9-29 10:38:11";
+  public static final String GIT_REVISION = "";
+}
